0c69968995269ad05d2d9d71a817c7f68f495c72
unman <unman@thirdeyesecurity.org>
2020-12-31 04:11:03 +0000

    Giving up on automagically using panda import

--- manager.py
+++ manager.py
@@ -10,13 +10,24 @@ Created on Sun Dec 20 10:35:19 2020
 
 import pandas as pd
 import networkx as nx
-import tkinter as tk
-from networkx_viewer import NodeToken, GraphCanvas
-from viewer import TkPassthroughViewerApp, ViewerApp
+from viewer import TkPassthroughViewerApp
+d = {}
+filelist = open('/home/user/list4','r').read().splitlines()
+for line in filelist:
+    (key, val) = line.split("|")
+    d[(key)] = val
+
+qubes = list(d.keys())
+netvms = list(d.values())
+df = pd.DataFrame({ 'from':qubes, 'to':netvms})
+
+df = pd.DataFrame({ 'from':qubes, 'to':netvms})
+G = nx.from_pandas_edgelist(df, 'from', 'to' )
+
 Viewer = TkPassthroughViewerApp
-#BasicViewer = ViewerApp
-df = pd.read_csv('/home/user/list',sep='|',header=None, names={'col','col1'},keep_default_na=False)
-G = nx.from_pandas_edgelist(df, source='col', target='col1')
+
+G.nodes['net-tablet']['color'] = 'grey'
+G.nodes['tor']['color'] = 'blue'
 
 app = Viewer(G)
 app.mainloop()
