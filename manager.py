7d7938c168789edcc6c13288b0443a1bfd34b24a
unman <unman@thirdeyesecurity.org>
2021-01-22 14:11:59 +0000

    Add capability to show template relationships. Add --all option to show all qubes including those with no netvm

--- manager.py
+++ manager.py
@@ -8,42 +8,53 @@ Created on Sun Dec 20 10:35:19 2020
 
 # libraries
 
+import argparse
 import pandas as pd
 import networkx as nx
 from viewer import TkPassthroughViewerApp
 
 import csv
-reader = csv.DictReader(open('/home/user/list11'), fieldnames=['qube','netvm','label','klass','template','IP','IPBACK','GATEWAY','visible_ip','visible_netmask','visible_gateway'],delimiter='|')
+reader = csv.DictReader(open('/home/user/list11'), fieldnames=['qube', 'netvm',
+                                'label', 'klass', 'template', 'IP', 'IPBACK',
+                                'GATEWAY', 'visible_ip', 'visible_netmask',
+                                'visible_gateway'], delimiter='|')
+parser = argparse.ArgumentParser(description="Display information about qubes network")
+parser.add_argument("--templates", help="Show template relationships",
+                    action="store_true")
+parser.add_argument("--all", help="Show network for all qubes including Templates",
+                    action="store_true")
+args = parser.parse_args()
 
 d = {}
 e = {}
 for row in reader:
     key = row.pop('qube')
     d[key] = row
-    
-for key,value in d.items():
-    if value['klass'] != 'TemplateVM':
-        e[key] = value['netvm']
-    else:
-        if value['netvm'] != '-':
+
+for key, value in d.items():
+    if args.templates:
+        if value['klass'] != 'TemplateVM':
+            e[key] = value['template']
+        else:
             e[key] = value['netvm']
-    
+    else:
+        if args.all:
+                e[key] = value['netvm']
+        else:
+            if value['netvm'] != '-':
+                e[key] = value['netvm']
+
 qubes = list(e.keys())
 netvms = list(e.values())
-df = pd.DataFrame({ 'from':qubes, 'to':netvms})
-G = nx.from_pandas_edgelist(df, 'from', 'to' )
+df = pd.DataFrame({'from': qubes, 'to': netvms})
+G = nx.from_pandas_edgelist(df, 'from', 'to')
 
 for qube in G.nodes:
     if qube != '-':
         G.nodes[qube]['color'] = d[qube]['label']
         G.nodes[qube]['Details'] = d[qube]
-        if d[qube]['klass'] == 'TemplateVM':
+        if (d[qube]['klass'] == 'TemplateVM') and (d[qube]['netvm'] != '-'):
             G.nodes[qube]['label_fill'] = 'red'
-        #if G.has_edge(qube,'-'):
-         #   G.remove_edge(qube,'-')
-
-#G.remove_edge('net-tablet','-')
 Viewer = TkPassthroughViewerApp
 app = Viewer(G)
 app.mainloop()
-
